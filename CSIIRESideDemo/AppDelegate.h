407ffa1a53c67c0a0a336c210361d4b746ff6948
Ritchie <1129984068@qq.com>
2017-05-08 11:23:30 +0800

    Initial Commit

---
+++ CSIIRESideDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CSIIRESideDemo
+//
+//  Created by xiangbiao on 17/5/8.
+//  Copyright © 2017年 xiangbiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CSIIRESideDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CSIIRESideDemo
+//
+//  Created by xiangbiao on 17/5/8.
+//  Copyright © 2017年 xiangbiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
